b1003df2728b3cce32b757e741315d329612d621
Ivan Filimonov <snake73w@gmail.com>
2018-07-15 22:29:10 +0700

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# SICP Racket
+
+### Requirements
+
+* Racket
+* Rackunit (`raco pkg install rackunit`)
+* Make
+
+### Run tests
+
+```sh
+$ make test
+```
---
+++ sicp/chapter1/03.rkt
@@ -0,0 +1,16 @@
+#lang racket/base
+
+(require rackunit)
+
+(define (sum-of-squares-max-two-numbers a b c)
+  (cond ((and (> a b) (> c b)) (sum-of-squares a c))
+        ((and (> a c) (> b c)) (sum-of-squares a b))
+        (else (sum-of-squares b c))))
+
+(define (sum-of-squares a b)
+  (+ (* a a) (* b b)))
+
+(check-equal? (sum-of-squares-max-two-numbers 0 0 0) 0)
+(check-equal? (sum-of-squares-max-two-numbers 3 2 1) 13)
+(check-equal? (sum-of-squares-max-two-numbers 10 5 9) 181)
+(check-equal? (sum-of-squares-max-two-numbers -2 -1 0) 1)
