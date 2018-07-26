f992b964fd64ff488c4c1f893708d8318019ca32
Ivan Filimonov <snake73w@gmail.com>
2018-07-26 18:38:12 +0700

    rename function in 1-12

--- sicp/chapter1/12.rkt
+++ sicp/chapter1/12.rkt
@@ -2,16 +2,16 @@
 
 (require rackunit)
 
-(define (f row col)
+(define (pascal-triangle row col)
   (if (or (= col 1) (= row col))
     1
-    (+ (f (- row 1) col)
-       (f (- row 1) (- col 1)))))
+    (+ (pascal-triangle (- row 1) col)
+       (pascal-triangle (- row 1) (- col 1)))))
 
-(check-equal? (f 1 1) 1)
-(check-equal? (f 2 1) 1)
-(check-equal? (f 3 1) 1)
-(check-equal? (f 5 5) 1)
-(check-equal? (f 3 2) 2)
-(check-equal? (f 5 2) 4)
-(check-equal? (f 5 3) 6)
+(check-equal? (pascal-triangle 1 1) 1)
+(check-equal? (pascal-triangle 2 1) 1)
+(check-equal? (pascal-triangle 3 1) 1)
+(check-equal? (pascal-triangle 5 5) 1)
+(check-equal? (pascal-triangle 3 2) 2)
+(check-equal? (pascal-triangle 5 2) 4)
+(check-equal? (pascal-triangle 5 3) 6)
