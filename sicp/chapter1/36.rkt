d1c4b00a950838d90d77dfa688967212d404407b
Ivan Filimonov <snake73w@gmail.com>
2019-04-23 20:58:18 +0700

    add solutions for exercises 1-35, 36

---
+++ sicp/chapter1/36.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    ((newline)
+     (display guess)
+     (let ((next (f guess)))
+       (if (close-enough? guess next)
+         next
+         (try next)))))
+  (try first-guess))
+
+(define xx
+  (fixed-point (lambda (x) (/ (log 1000) (log x)))
+               2.0))
+
+(check-equal? xx 4.555532270803653)
